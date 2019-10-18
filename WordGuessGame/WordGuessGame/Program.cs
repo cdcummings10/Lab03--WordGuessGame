20f1da2f67fe7d44da8bce0867596724ebc308b3
cdcummings10 <ccummings0913@gmail.com>
2019-10-18 08:39:53 -0700

    Finished testing for word add, remove, display word list and check character. refactored to test characters.

--- WordGuessGame/WordGuessGame/Program.cs
+++ WordGuessGame/WordGuessGame/Program.cs
@@ -13,7 +13,9 @@ namespace WordGuessGame
         {
             UserInterface();
         }
-
+        /// <summary>
+        /// Displays main menu for game. Runs functions based on user input in a switch statement.
+        /// </summary>
         public static void UserInterface()
         {
             string path = "../../../../docs/words.txt";
@@ -25,27 +27,37 @@ namespace WordGuessGame
                 Console.WriteLine("1) Start Game");
                 Console.WriteLine("2) Admin");
                 Console.WriteLine("3) Exit");
-                switch (Console.ReadLine())
+                try
                 {
-                    case "1":
-                        StartGame(path);
-                        break;
-                    case "2":
-                        AdminMenu(path);
-                        break;
-                    case "3":
-                        gameOn = false;
-                        ExitGame();
-                        break;
-                    default:
-                        Console.WriteLine("Please input valid menu number.");
-                        break;
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            StartGame(path);
+                            break;
+                        case "2":
+                            AdminMenu(path);
+                            break;
+                        case "3":
+                            gameOn = false;
+                            ExitGame();
+                            break;
+                        default:
+                            Console.WriteLine("Please input valid menu number.");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong! Please try again!");
                 }
             }
 
 
         }
-
+        /// <summary>
+        /// Displayed admin menu. Runs functions based on user input in a switch statement.
+        /// </summary>
+        /// <param name="path"></param>
         public static void AdminMenu(string path)
         {
             bool adminOn = true;
@@ -59,39 +71,57 @@ namespace WordGuessGame
                 Console.WriteLine("3) Remove a Word");
                 Console.WriteLine("4) Start Game");
                 Console.WriteLine("5) Exit");
-                switch (Console.ReadLine())
+
+                try
                 {
-                    case "1":
-                        Console.WriteLine(ViewWords(path));
-                        break;
-                    case "2":
-                        string addWord = Console.ReadLine();
-                        AddWord(path, addWord);
-                        break;
-                    case "3":
-                        string removeWord = Console.ReadLine();
-                        RemoveWords(path, removeWord);
-                        break;
-                    case "4":
-                        StartGame(path);
-                        break;
-                    case "5":
-                        ExitGame();
-                        break;
-                    default:
-                        Console.WriteLine("Please input valid menu number.");
-                        break;
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Console.WriteLine(ViewWords(path));
+                            break;
+                        case "2":
+                            string addWord = Console.ReadLine();
+                            AddWord(path, addWord);
+                            break;
+                        case "3":
+                            string removeWord = Console.ReadLine();
+                            RemoveWords(path, removeWord);
+                            break;
+                        case "4":
+                            StartGame(path);
+                            break;
+                        case "5":
+                            ExitGame();
+                            break;
+                        default:
+                            Console.WriteLine("Please input valid menu number.");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong! Please try again!");
                 }
             }
         }
-
+        /// <summary>
+        /// Takes in a file path and returns the words in the file as a string.
+        /// </summary>
+        /// <param name="path">Takes in a file path as a string.</param>
+        /// <returns>Returns word file as a string.</returns>
         public static string ViewWords(string path)
         {
             string wordsFile = File.ReadAllText(path);
             return wordsFile;
         }
-
-        public static void RemoveWords(string path, string word)
+        /// <summary>
+        /// Takes in a file path and a word to remove. Reads lines from the file path.
+        /// Iterates through array and returns a new array with the word removed. Rewrites .txt file.
+        /// </summary>
+        /// <param name="path">Takes in a file path as a string.</param>
+        /// <param name="word">Takes in a word to remove as a string.</param>
+        /// <returns>Returns new array of words with parameter word removed.</returns>
+        public static string[] RemoveWords(string path, string word)
         {
             string[] words = File.ReadAllLines(path);
             string[] newWords = new string[words.Length - 1];
@@ -112,19 +142,34 @@ namespace WordGuessGame
                 }
             }
             File.WriteAllLines(path, newWords);
+            return newWords;
         }
-
-        public static void AddWord(string path, string word)
+        /// <summary>
+        /// Takes in a file path and a word to add. Adds word to the end of file.
+        /// Returns array of the updated words list.
+        /// </summary>
+        /// <param name="path">Takes in a file path as a string.</param>
+        /// <param name="word">Takes in a word to add to file as a string.</param>
+        /// <returns>Returns an array of the updated word list.</returns>
+        public static string[] AddWord(string path, string word)
         {
             string[] lines = new string[] { word };
             File.AppendAllLines(path, lines);
+            return File.ReadAllLines(path);
         }
-
+        /// <summary>
+        /// Exits the game via Environment.Exit(0).
+        /// </summary>
         public static void ExitGame()
         {
             Environment.Exit(0);
         }
-
+        /// <summary>
+        /// Runs the main game. Grabs a random word from the stored words file and renders letters as 
+        /// underscores with spaces in between. Takes in user input and grabs first char, which is
+        /// then checked against all characters in the word. Ends game when all letters are guessed.
+        /// </summary>
+        /// <param name="path">Takes in a file path as a string.</param>
         public static void StartGame(string path)
         {
             string[] wordsList = File.ReadAllLines(path);
@@ -143,18 +188,11 @@ namespace WordGuessGame
 
                 try
                 {
-                char guess = Console.ReadLine()[0];
-                for (int i = 0; i < answerWordArray.Length; i++)
-                {
-                    if (Char.ToUpper(guess) == Char.ToUpper(answerWordArray[i]))
-                    {
-                        currentDisplay[i] = Char.ToUpper(answerWordArray[i]);
-                    }
+                    char guess = Console.ReadLine()[0];
+                    CheckWordForChar(guess, currentDisplay, answerWordArray);
                 }
-                }
-                catch (IndexOutOfRangeException e)
+                catch (IndexOutOfRangeException)
                 {
-
                     Console.WriteLine("Please enter a valid guess.");
                 }
                 catch (Exception)
@@ -169,6 +207,17 @@ namespace WordGuessGame
                     Console.WriteLine("Congratulations! You won!\n\n");
                 }
             }
+        }
+        public static char[] CheckWordForChar(char guess, char[] currentWord, char[] answerWord)
+        {
+            for (int i = 0; i < answerWord.Length; i++)
+            {
+                if (Char.ToUpper(guess) == Char.ToUpper(answerWord[i]))
+                {
+                    currentWord[i] = Char.ToUpper(answerWord[i]);
+                }
+            }
+            return currentWord;
         }
     }
 }
